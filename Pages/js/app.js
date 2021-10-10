774f9306549da2fabff15cba4ce6248fa0091987
hunumex <e.emandjo@gmail.com>
2021-10-10 18:29:42 -0300

    mise en place du navigateur (Menu)

---
+++ Pages/js/app.js
@@ -0,0 +1,5 @@
+const miniMenu = document.querySelector("#commuMenu");
+const commu = document.querySelector(".commu");
+
+commu.addEventListener('click', () => miniMenu.classList.toggle('menuVisible'));
+

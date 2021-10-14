90415e73a67bb9c7463ea554f88e5fe979237d34
hunumex <e.emandjo@gmail.com>
2021-10-14 01:22:18 -0300

    ajustement

--- Pages/js/app.js
+++ Pages/js/app.js
@@ -1,5 +1,10 @@
 const miniMenu = document.querySelector("#commuMenu");
 const commu = document.querySelector(".commu");
+const header = document.querySelector("#headerContent");
 
-commu.addEventListener('click', () => miniMenu.classList.toggle('menuVisible'));
+commu.addEventListener('click', () =>{
+     miniMenu.classList.toggle('menuVisible')
+    });
+
+
 
